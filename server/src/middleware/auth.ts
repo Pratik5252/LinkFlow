9a45b9cea2dba379fd51d46e793f369d730ae91d
Pratik Yesane <pratiky2000@gmail.com>
2025-06-04 13:30:14 +0530

    feat(middleware): add middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export interface CustomRequest extends Request {
+  user?: JwtPayload | string;
+}
+function authenticateToken(
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET! as string);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Token is not valid" });
+    return;
+  }
+}
+
+export default authenticateToken;
