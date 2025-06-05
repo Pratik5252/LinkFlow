96fae22ea7ef91034e1600e34439b605487d2c6d
Pratik Yesane <pratiky2000@gmail.com>
2025-06-05 18:14:42 +0530

    feat(service): add auth service

---
+++ client/src/services/auth.ts
@@ -0,0 +1,53 @@
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  message: string;
+  token?: string;
+  error?: string;
+}
+
+export interface RegisterPayload {
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  message: string;
+  user: {
+    email: string;
+    id: string;
+  };
+}
+
+export async function login(payload: LoginPayload): Promise<LoginResponse> {
+  const res = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (!res.ok) {
+    throw new Error("Login failed");
+  }
+
+  return res.json();
+}
+
+export async function register(
+  payload: RegisterPayload
+): Promise<RegisterResponse> {
+  const res = await fetch("http://localhost:3000/api/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  if (!res.ok) {
+    throw new Error("Login failed");
+  }
+
+  return res.json();
+}
