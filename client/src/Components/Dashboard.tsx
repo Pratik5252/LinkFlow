1dac4890692d2f960f48c1415a05369db8ad4b25
Pratik Yesane <pratiky2000@gmail.com>
2025-06-06 19:01:31 +0530

    feat: add dashboard

---
+++ client/src/Components/Dashboard.tsx
@@ -0,0 +1,14 @@
+import GetUrls from "./Urls/GetUrls";
+// import Navbar from "./Navbar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <GetUrls />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
