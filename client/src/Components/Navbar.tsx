dc3a814bb76aa8f5961978e311786ccbd35a6f60
Pratik Yesane <pratiky2000@gmail.com>
2025-06-06 19:02:13 +0530

    feat: add Navbar

---
+++ client/src/Components/Navbar.tsx
@@ -0,0 +1,31 @@
+import { logout } from "@/utils/logout";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+
+function Navbar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/auth");
+  };
+  return (
+    <div className="w-full">
+      <div className="w-screen flex justify-between px-4 py-2">
+        <div>
+          <p>Dashboard</p>
+        </div>
+        <div>
+          <Button
+            variant="outline"
+            className="cursor-pointer rounded-xs"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
