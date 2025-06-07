3e2e638ff71aac104e145b9fe15da4d90428b8dc
Pratik Yesane <pratiky2000@gmail.com>
2025-06-08 00:35:44 +0530

    feat: add createShortUrl service

--- client/src/services/url.ts
+++ client/src/services/url.ts
@@ -1,4 +1,4 @@
-import type { Url } from "@/types/url";
+import type { shortUrlPayload, shortUrlResponse, Url } from "@/types/url";
 import type { VisitResponse } from "@/types/visits";
 
 export const getUrls = async (): Promise<Url[]> => {
@@ -41,6 +41,33 @@ export const getUrlVisits = async (urlId: string): Promise<VisitResponse> => {
     }
 
     return res.json() as Promise<VisitResponse>;
+  } catch (error) {
+    throw new Error("Something went wrong");
+  }
+};
+
+export const createShortUrl = async (
+  payload: shortUrlPayload
+): Promise<shortUrlResponse> => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+  try {
+    const res = await fetch(`http://localhost:3000/api/url/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(payload),
+    });
+    
+    if (!res.ok) {
+      throw new Error("Failed to fetch visit data");
+    }
+
+    return res.json() as Promise<shortUrlResponse>;
   } catch (error) {
     throw new Error("Something went wrong");
   }
