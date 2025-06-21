98095f2a51abb88bc5bf61afc166a498c53d3edf
Pratik Yesane <pratiky2000@gmail.com>
2025-06-21 19:17:24 +0530

    feat: enhance GetUrls component with sorting, delete functionality, and improved UI layout

--- client/src/Components/Urls/GetUrls.tsx
+++ client/src/Components/Urls/GetUrls.tsx
@@ -1,13 +1,23 @@
-import { getUrls, getUrlVisits } from "@/services/url";
+import { deleteUrl, getUrls, getUrlVisits } from "@/services/url";
 import type { Url } from "@/types/url";
 import { useState } from "react";
-import Dropdown from "../Dropdown";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
-import { Plus } from "lucide-react";
-import { Link as URL } from "lucide-react";
+import {
+  Link as URL,
+  Plus,
+  Settings,
+  Trash,
+  Info,
+  ChevronDown,
+  ChevronUp,
+  ChevronsUpDown,
+} from "lucide-react";
 import ShortUrl from "./ShortUrl";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import Copy from "../Utils/Copy";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { useSortableData } from "@/hooks/useSortableData";
 
 const GetUrls = () => {
   const queryClient = useQueryClient();
@@ -17,13 +27,26 @@ const GetUrls = () => {
     data: urls = [],
     isLoading,
     error,
-    refetch,
   } = useQuery<Url[]>({
     queryKey: ["urls"],
     queryFn: getUrls,
     refetchInterval: 5000,
     staleTime: 5000,
   });
+
+  const { items, requestSort, getSortIndicator } = useSortableData(urls);
+
+  const renderSortIcon = (key: string) => {
+    const sortState = getSortIndicator(key);
+    switch (sortState) {
+      case "ascending":
+        return <ChevronUp size={12} />;
+      case "descending":
+        return <ChevronDown size={12} />;
+      default:
+        return <ChevronsUpDown size={12} />;
+    }
+  };
 
   const handleMouseEnter = (urlId: string) => {
     queryClient.prefetchQuery({
@@ -33,80 +56,151 @@ const GetUrls = () => {
     });
   };
 
+  const handleDelete = async (e, urlId) => {
+    e.stopPropagation();
+    await deleteUrl(urlId);
+  };
+
+  const formatDate = (date) => {
+    return new Date(date)
+      .toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      })
+      .replace(",", "");
+  };
+
   const handleUrl = () => {
     setOpen(true);
   };
 
   return (
-    <div className="max-w-6xl mx-auto mt-10 h-full w-full">
+    <div className="max-w-6xl mx-auto h-full w-full mt-4">
       {/* Table Header */}
-      <div className=" py-2 border-b text-secondary-foreground">
+      <div className=" py-2 text-foreground">
         <div className="flex justify-between">
-          <div className="flex gap-2 items-center text-xl font-semibold">
-            Your URLs{" "}
+          <div className="flex gap-2 items-center text-lg font-medium">
+            URLs{" "}
             <span>
               <URL size={20} />
             </span>
           </div>
-          <Button variant="link" onClick={handleUrl}>
+          <Button variant="default" onClick={handleUrl} className="rounded-xs">
             Add Url <Plus size={16} className="p-0 m-0" />{" "}
           </Button>
         </div>
       </div>
       {/* Table Rows */}
-      {isLoading && <div>Loading...</div>}
-      {error && <div>Error</div>}
-      {urls && (
-        <div className="mt-4 px-4">
-          {urls.map((url) => (
-            <Link
-              to={`/visit/${url.id}`}
-              key={url.shortLink}
-              onMouseEnter={() => handleMouseEnter(url.id)}
-            >
-              <div className="flex items-center px-4 py-4 rounded-md bg-gray-100 border my-2 text-secondary-foreground text-xs motion-safe:hover:scale-x-[1.01] duration-200 motion-safe:hover:shadow-2xs transition-all">
-                <div className="grid grid-cols-4 gap-4 grow">
-                  <div>
-                    <div className="text-gray-500 mb-0.5">Original URL</div>
-                    <a
-                      href={url.originalUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="truncate text-blue-500"
-                      onClick={(e) => e.stopPropagation()}
-                    >
-                      {url.originalUrl}
-                    </a>
-                  </div>
-                  <div>
-                    <div className="text-gray-500 mb-0.5">Short URL</div>
-                    <a
-                      href={url.shortLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="truncate text-blue-500"
-                      onClick={(e) => e.stopPropagation()}
-                    >
-                      {url.shortLink}
-                    </a>
-                  </div>
-                  <div>
-                    <div className="text-gray-500 mb-0.5">Visits</div>
-                    <p>{url._count.visits}</p>
-                  </div>
-                  <div>
-                    <div className="text-gray-500 mb-0.5">Created At</div>
-                    <p>{url.createdAt.split("T")[0]}</p>
+
+      <div className="mt-4 rounded-lg overflow-hidden ">
+        <div className="table-grid bg-background text-secondary-foreground  text-sm font-light">
+          <div className="px-4 py-3">Short URL</div>
+          <div className="px-4 py-3">Original URL</div>
+          <div className="px-4 py-3 flex items-center justify-center gap-1">
+            <p>Analytics</p>
+            <Tooltip>
+              <TooltipTrigger className="h-full mt-0.5">
+                <Info size={12} strokeWidth={1} />
+              </TooltipTrigger>
+              <TooltipContent className="border bg-accent text-accent-foreground text-xs">
+                To enable analytics, go to the URL settings and verify your
+                website ownership.
+              </TooltipContent>
+            </Tooltip>
+          </div>
+          <div
+            className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
+            onClick={() => requestSort("_count.visits")}
+          >
+            Visits
+            {renderSortIcon("_count.visits")}
+          </div>
+          <div
+            className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
+            onClick={() => requestSort("createdAt")}
+          >
+            Date <span>{renderSortIcon("createdAt")}</span>
+          </div>
+          <div className="px-4 py-3 whitespace-nowrap flex items-center justify-center"></div>
+        </div>
+
+        {isLoading && <div>Loading...</div>}
+        {error && <div>Error</div>}
+        {urls && (
+          <div className="">
+            {items.map((url) => (
+              <div
+                className=" bg-background text-xs text-foreground border-t table-grid"
+                key={url.originalUrl}
+              >
+                <div className="flex items-center px-4 py-4 gap-2">
+                  <Link
+                    to={url.shortLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="truncate"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    {url.shortLink}
+                  </Link>
+                  <div className="flex items-center">
+                    <Copy shortUrl={url.shortLink} />
                   </div>
                 </div>
-                <div className="flex-none">
-                  <Dropdown urlId={url.id} />
+
+                <div className="flex items-center px-4 py-4">
+                  <Link
+                    to={url.originalUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="truncate"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    {url.originalUrl}
+                  </Link>
+                </div>
+
+                <div className=" px-4 py-4 flex items-center justify-center">
+                  <Link
+                    to={`/visit/${url.id}`}
+                    key={url.shortLink}
+                    onMouseEnter={() => handleMouseEnter(url.id)}
+                    className="hover:text-secondary-foreground transition-all duration-200"
+                  >
+                    {" "}
+                    View
+                  </Link>
+                </div>
+
+                <div className="px-4 py-4 flex items-center justify-center">
+                  <p>{url._count.visits}</p>
+                </div>
+
+                <div className="px-4 py-4 flex items-center justify-center">
+                  <p>{formatDate(url.createdAt)}</p>
+                </div>
+
+                <div className="flex items-center justify-center px-4 py-4 gap-4">
+                  <button className="cursor-pointer">
+                    <Settings
+                      size={20}
+                      strokeWidth={1}
+                      className="hover:rotate-45 transition-transform duration-300"
+                    />
+                  </button>
+                  <button
+                    onClick={(e) => handleDelete(e, url.id)}
+                    className="p-1 cursor-pointer justify-between hover:text-red-400"
+                  >
+                    <Trash size={20} strokeWidth={1} />
+                  </button>
                 </div>
               </div>
-            </Link>
-          ))}
-        </div>
-      )}
+            ))}
+          </div>
+        )}
+      </div>
       <ShortUrl open={open} onOpenChange={setOpen} />
     </div>
   );
