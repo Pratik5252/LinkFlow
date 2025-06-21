97af9d680332f05f3b98b9762f247089fcc38d6f
Pratik Yesane <pratiky2000@gmail.com>
2025-06-21 19:19:17 +0530

    feat: add Logout component for user logout functionality with navigation to authentication page

---
+++ client/src/Components/Utils/Logout.tsx
@@ -0,0 +1,23 @@
+import { logout } from "@/utils/logout";
+import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+
+const Logout = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/auth");
+  };
+  return (
+    <Button
+      variant="ghost"
+      className="cursor-pointer rounded-xs w-full flex items-center justify-between"
+      onClick={handleLogout}
+    >
+      Logout <LogOut />
+    </Button>
+  );
+};
+
+export default Logout;
