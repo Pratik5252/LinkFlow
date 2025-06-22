d51ae70e5ad52b95a62ecda67a753f28cc2d1fd5
Pratik Yesane <pratiky2000@gmail.com>
2025-06-23 00:28:13 +0530

    feat: integrate Toaster component for notifications with theme support in Layout

--- client/src/Components/Layout.tsx
+++ client/src/Components/Layout.tsx
@@ -2,15 +2,20 @@ import { SidebarInset, SidebarProvider } from "@/Components/ui/sidebar";
 import { AppSidebar } from "@/Components/AppSidebar";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import type { ToasterProps } from "sonner";
+import { Toaster } from "./ui/sonner";
+import { useTheme } from "next-themes";
 
 export default function Layout() {
+  const { resolvedTheme } = useTheme();
   return (
     <SidebarProvider>
       <AppSidebar />
       {/* <SidebarInset> */}
-      <main className="w-full h-fit">
+      <main className="w-full h-full">
         <Navbar />
         <Outlet />
+        <Toaster richColors theme={resolvedTheme as ToasterProps["theme"]} />
       </main>
       {/* </SidebarInset> */}
     </SidebarProvider>
