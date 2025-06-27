48cb03b9efd05754f4fc2df5b13cf8c7bb0acb3a
Pratik Yesane <pratiky2000@gmail.com>
2025-06-27 12:35:21 +0530

    feat: implement Google login functionality in AuthFormBase and enhance error handling in authentication services

--- client/src/Components/Auth/AuthFormBase.tsx
+++ client/src/Components/Auth/AuthFormBase.tsx
@@ -10,6 +10,10 @@ import {
 import { Input } from "@/Components/ui/input";
 import { Label } from "@/Components/ui/label";
 import { useState } from "react";
+import { signInWithGoogle } from "@/services/auth";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 interface AuthFormBaseProps {
   title: string;
@@ -18,7 +22,7 @@ interface AuthFormBaseProps {
   submitLabel: string;
   switchLabel: string;
   onSwitch: () => void;
-  error?: string | null;
+  error?: Error | null;
 }
 
 export function AuthFormBase({
@@ -32,10 +36,33 @@ export function AuthFormBase({
 }: AuthFormBaseProps) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validationError, setValidationError] = useState<string | null>("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setValidationError(null);
+    if (submitLabel === "Register" && password !== confirmPassword) {
+      return;
+    }
     await onSubmit(email, password);
+  };
+
+  const { mutateAsync } = useMutation({
+    mutationFn: signInWithGoogle,
+    onSuccess: (data) => {
+      localStorage.setItem("token", data.token);
+      navigate("/");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const handleGoogleLogin = async () => {
+    mutateAsync();
   };
 
   return (
@@ -69,21 +96,62 @@ export function AuthFormBase({
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              {error && <div className="text-red-500 text-xs">{error}</div>}
-              <Button variant="default" type="submit" className="w-full cursor-pointer">
+              {submitLabel === "Register" && (
+                <div className="grid gap-3">
+                  <Label htmlFor="confirm-password">Confirm Password</Label>
+                  <Input
+                    id="confirm-password"
+                    type="password"
+                    required
+                    value={confirmPassword}
+                    onChange={(e) => {
+                      setConfirmPassword(e.target.value);
+                      setValidationError(null);
+                    }}
+                  />
+                </div>
+              )}
+              {(validationError || error) && (
+                <div className="text-red-500 text-xs -mt-4">
+                  {validationError || error?.message}
+                </div>
+              )}
+              <Button
+                variant="default"
+                type="submit"
+                className="w-full cursor-pointer"
+              >
                 {submitLabel}
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
-              <button
-                type="button"
-                className="underline underline-offset-4 cursor-pointer"
-                onClick={onSwitch}
-              >
-                {switchLabel}
-              </button>
-            </div>
           </form>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t mt-4">
+            <span className="bg-background text-muted-foreground relative z-10 px-2">
+              Or continue with
+            </span>
+          </div>
+          <Button
+            variant="outline"
+            className="w-full mt-4"
+            onClick={handleGoogleLogin}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                fill="currentColor"
+              />
+            </svg>
+            Login with Google
+          </Button>
+          <div className="mt-4 text-center text-sm">
+            <button
+              type="button"
+              className="underline underline-offset-4 cursor-pointer"
+              onClick={onSwitch}
+            >
+              {switchLabel}
+            </button>
+          </div>
         </CardContent>
       </Card>
     </div>
--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,3 +1,6 @@
+import { auth } from "@/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
 export interface LoginPayload {
   email: string;
   password: string;
@@ -25,31 +28,67 @@ export interface RegisterResponse {
 const API_URL = import.meta.env.VITE_API_URL;
 
 export async function login(payload: LoginPayload): Promise<LoginResponse> {
-  const res = await fetch(`${API_URL}/auth/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  });
+  try {
+    const res = await fetch(`${API_URL}/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
 
-  if (!res.ok) {
-    throw new Error("Login failed");
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "Login failed");
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(error.message || "Something went wrong");
   }
-
-  return res.json();
 }
 
 export async function register(
   payload: RegisterPayload
 ): Promise<RegisterResponse> {
-  const res = await fetch(`${API_URL}/auth/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  });
+  try {
+    const res = await fetch(`${API_URL}/auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
 
-  if (!res.ok) {
-    throw new Error("Login failed");
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.message || "Registration failed");
+    }
+
+    return res.json();
+  } catch (error) {
+    throw new Error(error.message || "Something went wrong");
   }
-
-  return res.json();
 }
+
+export const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  console.log(result);
+
+  const idToken = await result.user.getIdToken();
+
+  try {
+    const res = await fetch(`${API_URL}/auth/google`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ idToken }),
+    });
+
+    if (!res.ok) {
+      throw new Error("Login failed");
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    throw new Error(error.message || "Something went wrong");
+  }
+};
