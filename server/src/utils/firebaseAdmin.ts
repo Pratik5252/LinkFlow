7d2b3bf9eed390b8474a8dc833d1a75f9246214c
Pratik Yesane <pratiky2000@gmail.com>
2025-06-27 13:48:11 +0530

    refactor: update Firebase service account import to use file system read for improved compatibility

--- server/src/utils/firebaseAdmin.ts
+++ server/src/utils/firebaseAdmin.ts
@@ -1,6 +1,13 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../config/serviceAccountKey.json" assert {type: "json"};
+import { readFileSync } from "fs";
+
+const serviceAccount = JSON.parse(
+  readFileSync(
+    new URL("../config/serviceAccountKey.json", import.meta.url),
+    "utf-8"
+  )
+);
 
 const app = initializeApp({
   credential: cert(serviceAccount as any),
