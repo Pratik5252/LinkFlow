c96c9dbe409d87e86e840cd099e2be16119d05bb
Pratik Yesane <pratiky2000@gmail.com>
2025-07-13 23:56:09 +0530

    feat: add Delete utility component for handling URL deletions with dialog confirmation

---
+++ client/src/Components/Utils/Delete.tsx
@@ -0,0 +1,55 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { Trash } from "lucide-react";
+import type { Url } from "@/types/url";
+import DeleteUrl from "../Urls/DeleteUrl";
+
+const Delete = ({ url, deleteDialog, setDeleteDialog }) => {
+  const queryClient = useQueryClient();
+
+  const handleDeleteClick = (url: Url) => {
+    setDeleteDialog({
+      isOpen: true,
+      url: url,
+    });
+  };
+
+  const handleDeleteDialogClose = (isOpen: boolean) => {
+    if (!isOpen) {
+      setDeleteDialog({
+        isOpen: false,
+        url: null,
+      });
+    }
+  };
+
+  const handleDeleteSuccess = () => {
+    queryClient.invalidateQueries({ queryKey: ["urls"] });
+    setDeleteDialog({
+      isOpen: false,
+      url: null,
+    });
+  };
+  return (
+    <>
+      <button
+        onClick={() => handleDeleteClick(url)}
+        className="p-1 cursor-pointer justify-between hover:text-red-400"
+      >
+        <Trash size={20} strokeWidth={1} />
+      </button>
+      {deleteDialog.url && (
+        <DeleteUrl
+          open={deleteDialog.isOpen}
+          onOpenChange={handleDeleteDialogClose}
+          onDeleteSuccess={handleDeleteSuccess}
+          urlId={deleteDialog.url.id}
+          shortLink={deleteDialog.url.shortLink}
+          originalUrl={deleteDialog.url.originalUrl}
+          title={deleteDialog.url.title ?? ""}
+        />
+      )}
+    </>
+  );
+};
+
+export default Delete;
