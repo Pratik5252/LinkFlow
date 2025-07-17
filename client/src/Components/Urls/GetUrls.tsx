438427dc2685d77baa53271ced6e73b2e1d8d4e0
Pratik Yesane <pratiky2000@gmail.com>
2025-07-18 01:00:47 +0530

    feat: enhance GetUrls component with loading skeletons, no data state, and share functionality

--- client/src/Components/Urls/GetUrls.tsx
+++ client/src/Components/Urls/GetUrls.tsx
@@ -8,8 +8,8 @@ import {
   ChevronDown,
   ChevronUp,
   ChevronsUpDown,
+  Plus,
 } from "lucide-react";
-
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Copy from "../Utils/Copy";
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
@@ -17,11 +17,15 @@ import { useSortableData } from "@/hooks/useSortableData";
 import CreateShortUrl from "./CreateShortUrl";
 import { useState } from "react";
 import Delete from "../Utils/Delete";
+import Share from "../Utils/Share";
+import { Skeleton } from "../ui/skeleton";
+import { NoData } from "@/assets/illustrations/NoData";
+import { Button } from "../ui/button";
 
 const GetUrls = () => {
-  const [deleteDialog, setDeleteDialog] = useState<{
+  const [dialog, setDialog] = useState<{
     isOpen: boolean;
-    url: Url | null;
+    url?: Url | null;
   }>({
     isOpen: false,
     url: null,
@@ -72,57 +76,104 @@ const GetUrls = () => {
       .replace(",", "");
   };
 
-  return (
-    <div className="max-w-6xl mx-auto h-full w-full mt-4">
-      <div className="">
-        <CreateShortUrl />
-      </div>
-      {/* Table Header */}
-      <div className=" py-2 text-foreground mt-4">
-        <div className="flex justify-between">
-          <div className="flex gap-2 items-center text-lg font-medium">
-            URLs{" "}
-            <span>
-              <URL size={20} />
-            </span>
+  const PageLoading = () => {
+    return (
+      <div>
+        {[...Array(5)].map((_, i) => (
+          <div
+            className="bg-background text-xs text-foreground border-t table-grid"
+            key={i}
+          >
+            <div className="flex items-center px-4 py-4 gap-2">
+              <Skeleton className="h-4 w-32" />
+            </div>
+            <div className="flex items-center px-4 py-4">
+              <Skeleton className="h-4 w-48" />
+            </div>
+            <div className="px-4 py-4 flex items-center justify-center">
+              <Skeleton className="h-4 w-12" />
+            </div>
+            <div className="px-4 py-4 flex items-center justify-center">
+              <Skeleton className="h-4 w-8" />
+            </div>
+            <div className="px-4 py-4 flex items-center justify-center">
+              <Skeleton className="h-4 w-20" />
+            </div>
+            <div className="flex items-center justify-center px-4 py-4 gap-4">
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-6 w-6 rounded-full" />
+              <Skeleton className="h-6 w-6 rounded-full" />
+            </div>
           </div>
-        </div>
+        ))}
       </div>
+    );
+  };
+
+  return (
+    <div className="max-w-6xl mx-8 h-full w-full mt-4">
+      {/* Table Header */}
+      {urls.length != 0 ? (
+        <div className=" py-2 text-foreground mt-4 flex justify-between">
+          <div className="flex justify-between">
+            <div className="flex gap-2 items-center text-lg font-medium">
+              URLs{" "}
+              <span>
+                <URL size={20} />
+              </span>
+            </div>
+          </div>
+          <CreateShortUrl text="Create" />
+        </div>
+      ) : null}
       {/* Table Rows */}
 
-      <div className="mt-4 rounded-lg overflow-hidden ">
-        <div className="table-grid bg-background text-secondary-foreground  text-sm font-light">
-          <div className="px-4 py-3">Short URL</div>
-          <div className="px-4 py-3">Original URL</div>
-          <div className="px-4 py-3 flex items-center justify-center gap-1">
-            <p>Analytics</p>
-            <Tooltip>
-              <TooltipTrigger className="h-full mt-0.5">
-                <Info size={12} strokeWidth={1} />
-              </TooltipTrigger>
-              <TooltipContent className="border text-xs">
-                To enable analytics, go to the URL settings and verify your
-                website ownership.
-              </TooltipContent>
-            </Tooltip>
+      <div className="mt-4 rounded-lg overflow-hidden h-full">
+        {urls.length > 0 && (
+          <div className="table-grid bg-background text-secondary-foreground  text-sm font-light">
+            <div className="px-4 py-3">Short URL</div>
+            <div className="px-4 py-3">Original URL</div>
+            <div className="px-4 py-3 flex items-center justify-center gap-1">
+              <p>Analytics</p>
+              <Tooltip>
+                <TooltipTrigger className="h-full mt-0.5">
+                  <Info size={12} strokeWidth={1} />
+                </TooltipTrigger>
+                <TooltipContent className="border text-xs">
+                  To enable analytics, go to the URL settings and verify your
+                  website ownership.
+                </TooltipContent>
+              </Tooltip>
+            </div>
+            <div
+              className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
+              onClick={() => requestSort("_count.visits")}
+            >
+              Visits
+              {renderSortIcon("_count.visits")}
+            </div>
+            <div
+              className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
+              onClick={() => requestSort("createdAt")}
+            >
+              Date <span>{renderSortIcon("createdAt")}</span>
+            </div>
+            <div className="px-4 py-3 whitespace-nowrap flex items-center justify-center"></div>
           </div>
-          <div
-            className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
-            onClick={() => requestSort("_count.visits")}
-          >
-            Visits
-            {renderSortIcon("_count.visits")}
+        )}
+        {urls.length == 0 && !isLoading && (
+          <div className="w-full h-full min-h-[300px] flex flex-col justify-center items-center gap-2">
+            <p className="text-muted-foreground font-bold">Nothing here yet!</p>
+            <NoData />
+            <div className="flex flex-col justify-center items-center mt-4 gap-2">
+              <p className="text-muted-foreground text-sm">
+                Create your first short link!
+              </p>
+              <CreateShortUrl text="Create" />
+            </div>
           </div>
-          <div
-            className="px-4 py-3 whitespace-nowrap flex items-center justify-center gap-1"
-            onClick={() => requestSort("createdAt")}
-          >
-            Date <span>{renderSortIcon("createdAt")}</span>
-          </div>
-          <div className="px-4 py-3 whitespace-nowrap flex items-center justify-center"></div>
-        </div>
-
-        {isLoading && <div>Loading...</div>}
+        )}
+        {isLoading && <PageLoading />}
         {error && <div>Error</div>}
         {urls && (
           <div className="">
@@ -142,7 +193,7 @@ const GetUrls = () => {
                     {url.shortLink}
                   </Link>
                   <div className="flex items-center">
-                    <Copy shortUrl={url.shortLink} />
+                    <Copy url={url.shortLink} />
                   </div>
                 </div>
 
@@ -194,9 +245,10 @@ const GetUrls = () => {
                   </button> */}
                   <Delete
                     url={url}
-                    deleteDialog={deleteDialog}
-                    setDeleteDialog={setDeleteDialog}
+                    deleteDialog={dialog}
+                    setDeleteDialog={setDialog}
                   />
+                  <Share value={url.shortLink} />
                 </div>
               </div>
             ))}
