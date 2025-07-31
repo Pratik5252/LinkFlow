8f2b1ee28d7cdfd829082d4bbddcc1ab6f0510be
Pratik Yesane <pratiky2000@gmail.com>
2025-07-31 23:22:13 +0530

    refactor: improve code formatting and indentation in getAllUrls function

--- server/src/controllers/url/getUrl.ts
+++ server/src/controllers/url/getUrl.ts
@@ -1,29 +1,36 @@
-import { Response } from "express";
-import { CustomRequest } from "../../middleware/auth.js";
-import { JwtPayload } from "jsonwebtoken";
-import prisma from "../../prisma/prismaClient.js";
+import { Response } from 'express';
+import { CustomRequest } from '../../middleware/auth.js';
+import { JwtPayload } from 'jsonwebtoken';
+import prisma from '../../prisma/prismaClient.js';
 
 const baseUrl = process.env.BASE_URL;
 export const getAllUrls = async (req: CustomRequest, res: Response) => {
-  const userId = (req.user as JwtPayload & { userId: string }).userId;
+    const userId = (req.user as JwtPayload & { userId: string }).userId;
 
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      include: { urls: { include: { _count: { select: { visits: true } } } } },
-    });
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            include: {
+                urls: {
+                    include: {
+                        _count: { select: { visits: true } },
+                        visits: { select: { timestamp: true } }, // Include visit timestamps for percentage calculations
+                    },
+                },
+            },
+        });
 
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-      return;
+        if (!user) {
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+        const urlsWithShotLink = user.urls.map((url: any) => ({
+            ...url,
+            shortLink: `${baseUrl}/${url.shortUrl}`,
+        }));
+
+        res.json(urlsWithShotLink);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-    const urlsWithShotLink = user.urls.map((url: any) => ({
-      ...url,
-      shortLink: `${baseUrl}/${url.shortUrl}`,
-    }));
-
-    res.json(urlsWithShotLink);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
 };
