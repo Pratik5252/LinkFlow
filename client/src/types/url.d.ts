ca1639fb887493ff7e0606d389a7d9d11058f902
Pratik Yesane <pratiky2000@gmail.com>
2025-07-31 23:21:54 +0530

    refactor: standardize indentation and formatting in Url type definitions

--- client/src/types/url.d.ts
+++ client/src/types/url.d.ts
@@ -1,28 +1,31 @@
 export interface Url {
-  id: string;
-  userId: string;
-  originalUrl: string;
-  shortUrl: string;
-  title: string | null;
-  createdAt: string;
-  _count: {
-    visits: number;
-  };
-  shortLink: string;
+    id: string;
+    userId: string;
+    originalUrl: string;
+    shortUrl: string;
+    title: string | null;
+    createdAt: string;
+    _count: {
+        visits: number;
+    };
+    visits: {
+        timestamp: string;
+    }[];
+    shortLink: string;
 }
 
 export interface shortUrlPayload {
-  originalUrl: string;
-  customUrl?: string;
-  title?: string;
+    originalUrl: string;
+    customUrl?: string;
+    title?: string;
 }
 
 export interface shortUrlResponse {
-  id: string;
-  userId: string;
-  originalUrl: string;
-  shortUrl: string;
-  title?: string;
-  createdAt: string;
-  shortLink: string;
+    id: string;
+    userId: string;
+    originalUrl: string;
+    shortUrl: string;
+    title?: string;
+    createdAt: string;
+    shortLink: string;
 }
