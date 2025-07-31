8be573938323f1efe46bdaf84a9d7e9f602c3a23
Pratik Yesane <pratiky2000@gmail.com>
2025-08-01 00:40:41 +0530

    refactor: update getUrls and related functions to support pagination and improve error handling

--- client/src/services/url.ts
+++ client/src/services/url.ts
@@ -1,103 +1,135 @@
-import type { shortUrlPayload, shortUrlResponse, Url } from "@/types/url";
-import type { VisitResponse } from "@/types/visits";
+import type {
+    shortUrlPayload,
+    shortUrlResponse,
+    Url,
+    PaginatedUrlsResponse,
+} from '@/types/url';
+import type { VisitResponse } from '@/types/visits';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getUrls = async (): Promise<Url[]> => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error("Unauthorized");
-  }
-  try {
-    const res = await fetch(`${API_URL}/url/`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
+export const getUrls = async (
+    page: number = 1,
+    limit: number = 10
+): Promise<PaginatedUrlsResponse> => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error('Unauthorized');
     }
+    try {
+        const res = await fetch(`${API_URL}/url/?page=${page}&limit=${limit}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
 
-    return res.json() as Promise<Url[]>;
-  } catch (error) {
-    throw new Error("Something went wrong");
-  }
+        return res.json();
+    } catch (error) {
+        throw new Error('Something went wrong');
+    }
+};
+
+export const getUrlsWithMetrics = async (): Promise<Url[]> => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error('Unauthorized');
+    }
+    try {
+        const res = await fetch(`${API_URL}/url/metrics`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        return res.json() as Promise<Url[]>;
+    } catch (error) {
+        throw new Error('Something went wrong');
+    }
 };
 
 export const getUrlVisits = async (urlId: string): Promise<VisitResponse> => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error("Unauthorized");
-  }
-  try {
-    const res = await fetch(`${API_URL}/url/${urlId}/visits`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!res.ok) {
-      throw new Error("Failed to fetch visit data");
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error('Unauthorized');
     }
+    try {
+        const res = await fetch(`${API_URL}/url/${urlId}/visits`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch visit data');
+        }
 
-    return res.json() as Promise<VisitResponse>;
-  } catch (error) {
-    throw new Error("Something went wrong");
-  }
+        return res.json() as Promise<VisitResponse>;
+    } catch (error) {
+        throw new Error('Something went wrong');
+    }
 };
 
 export const createShortUrl = async (
-  payload: shortUrlPayload
+    payload: shortUrlPayload
 ): Promise<shortUrlResponse> => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error("Unauthorized");
-  }
-  try {
-    const res = await fetch(`${API_URL}/url/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(payload),
-    });
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(
-        data.message || data.error || "Failed to create short URL"
-      );
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error('Unauthorized');
     }
+    try {
+        const res = await fetch(`${API_URL}/url/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(payload),
+        });
+        const data = await res.json();
 
-    return data as shortUrlResponse;
-  } catch (error) {
-    throw new Error(error.message || "Something went wrong");
-  }
+        if (!res.ok) {
+            throw new Error(
+                data.message || data.error || 'Failed to create short URL'
+            );
+        }
+
+        return data as shortUrlResponse;
+    } catch (error) {
+        throw new Error(error.message || 'Something went wrong');
+    }
 };
 
 export const deleteUrl = async (urlId: string) => {
-  const token = localStorage.getItem("token");
-  if (!token) {
-    throw new Error("Unauthorized");
-  }
-  try {
-    const res = await fetch(`${API_URL}/url/${urlId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(data.message || data.error || "Failed to delete teh Url");
+    const token = localStorage.getItem('token');
+    if (!token) {
+        throw new Error('Unauthorized');
     }
-    return data;
-  } catch (error) {
-    throw new Error(error.message || "Something went wrong");
-  }
+    try {
+        const res = await fetch(`${API_URL}/url/${urlId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(
+                data.message || data.error || 'Failed to delete teh Url'
+            );
+        }
+        return data;
+    } catch (error) {
+        throw new Error(error.message || 'Something went wrong');
+    }
 };
