1bd6a45b580d8cf651e8c7308ef1126999a433b7
Pratik Yesane <pratiky2000@gmail.com>
2025-08-08 22:22:09 +0530

    refactor: update Dashboard component for improved styling and layout

--- client/src/Components/Dashboard/Dashboard.tsx
+++ client/src/Components/Dashboard/Dashboard.tsx
@@ -1,12 +1,12 @@
-import GetUrls from "../Urls/GetUrls";
+import GetUrls from '../Urls/GetUrls';
 // import Navbar from "./Navbar";
 
 const Dashboard = () => {
-  return (
-    <div className="w-full h-full bg-background flex justify-center items-center">
-      <GetUrls />
-    </div>
-  );
+    return (
+        <div className="w-full h-fit bg-background flex justify-center items-center rounded-b-2xl">
+            <GetUrls />
+        </div>
+    );
 };
 
 export default Dashboard;
