49a855b904e659d719b92c8658b5d47fb133d85b
Pratik Yesane <pratiky2000@gmail.com>
2025-08-21 17:25:37 +0530

    refactor: Standardize import formatting and add logout route in auth router

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,15 +1,20 @@
-import { Router } from "express";
-import { register } from "../controllers/auth/register.js";
-import { getMeContoller } from "../controllers/auth/getMeController.js";
-import authenticateToken from "../middleware/auth.js";
-import { login } from "../controllers/auth/login.js";
-import { googleAuth } from "../controllers/auth/googleAuth.js";
+import { Router } from 'express';
+import { register } from '../controllers/auth/register.js';
+import { getMeContoller } from '../controllers/auth/getMeController.js';
+import authenticateToken from '../middleware/auth.js';
+import { login } from '../controllers/auth/login.js';
+import { logout } from '../controllers/auth/logout.js';
+import { googleAuth } from '../controllers/auth/googleAuth.js';
+import { deleteUser } from '../controllers/user/deleteUser.js';
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/google", googleAuth);
-router.get("/me", authenticateToken, getMeContoller);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/google', googleAuth);
+router.get('/me', authenticateToken, getMeContoller);
+
+router.delete('/user', authenticateToken, deleteUser);
 
 export default router;
