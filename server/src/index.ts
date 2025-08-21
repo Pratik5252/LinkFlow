04602d6870cdd02beafff496256827ab6c4fd2b4
Pratik Yesane <pratiky2000@gmail.com>
2025-08-21 17:26:31 +0530

    refactor: Improve code formatting and indentation in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,30 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/auth.js";
-import urlRoutes from "./routes/url.js";
-import { redirectUrl } from "./controllers/url/redirectUrl.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.js';
+import urlRoutes from './routes/url.js';
+import { redirectUrl } from './controllers/url/redirectUrl.js';
 
 dotenv.config();
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
 );
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/url", urlRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/url', urlRoutes);
 
-app.get("/:shorturl", redirectUrl);
+app.get('/:shorturl', redirectUrl);
 
-app.listen(3000,'0.0.0.0', () => {
-  console.log("Server is running on port 3000");
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
 });
