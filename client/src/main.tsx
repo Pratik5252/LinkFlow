64039d61522bc015ae1a7133f3f9b2f8f4b59ee2
Pratik Yesane <pratiky2000@gmail.com>
2025-08-21 17:34:36 +0530

    chore: Comment out ReactQueryDevtools in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
