0fca99add10075a2a15156a6501cbf5ebff75602
Pratik Yesane <pratiky2000@gmail.com>
2025-09-20 21:40:17 +0530

    chore: Comment out ReactQueryDevtools import for cleaner code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import "./App.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 const queryClient = new QueryClient();
