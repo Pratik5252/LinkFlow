dfcb2cc45939268a509ed47900b8f02e7453a41c
Pratik Yesane <pratiky2000@gmail.com>
2025-09-22 17:27:33 +0530

    fix: update CORS origin to allow requests from production URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173', 'https://link-flow-nine.vercel.app/'],
         credentials: true,
     })
 );
