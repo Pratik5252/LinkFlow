50b6678cfe6a12a70550dd55b7456ffbabfc8870
Pratik Yesane <pratiky2000@gmail.com>
2025-09-22 16:55:40 +0530

    fix: add root route to respond with server status

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,6 +25,10 @@ app.use('/api/url', urlRoutes);
 
 app.get('/:shorturl', redirectUrl);
 
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
 app.listen(PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
