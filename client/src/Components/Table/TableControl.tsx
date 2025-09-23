ebd9534739fa1a68990f7123c711e7188beedc24
Pratik Yesane <pratiky2000@gmail.com>
2025-09-23 14:20:06 +0530

    refactor: enhance TableControl and TableRows components for improved layout and functionality

--- client/src/Components/Table/TableControl.tsx
+++ client/src/Components/Table/TableControl.tsx
@@ -29,46 +29,54 @@ const TableControl = ({
     handleLimitChange,
 }: TableControlProps) => {
     return (
-        <div className="py-1 text-foreground mt-4 flex justify-between">
-            <div className="flex justify-between items-center gap-4">
-                <div className="flex gap-2 items-center text-lg font-medium">
-                    URLs{' '}
-                    <span>
-                        <URL size={20} />
-                    </span>
-                </div>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                        <Search className="h-4 w-4 text-muted-foreground" />
-                    </div>
-                    <Input
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                        placeholder="Search URLs..."
-                        className="pl-9 pr-9 w-64"
-                    />
-                    {search && (
-                        <button
-                            onClick={() => {
-                                setSearch('');
-                                setSearchQuery('');
-                            }}
-                            className="absolute inset-y-0 right-0 pr-3 flex items-center hover:text-foreground text-muted-foreground transition-colors"
-                        >
-                            <X className="h-4 w-4" />
-                        </button>
-                    )}
-                </div>
-                {pagination && (
-                    <div className="text-sm text-muted-foreground">
-                        Showing {(pagination.currentPage - 1) * limit + 1} to{' '}
-                        {Math.min(
-                            pagination.currentPage * limit,
-                            pagination.totalCount
+        <div className="py-1 text-foreground mt-4 flex justify-between items-start">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+                <div>
+                    {/* <div className="flex gap-2 items-center text-lg font-medium">
+                        URLs{' '}
+                        <span>
+                            <URL size={20} />
+                        </span>
+                    </div> */}
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <Search className="h-4 w-4 text-muted-foreground" />
+                        </div>
+                        <Input
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            placeholder="Search URLs..."
+                            className="pl-9 pr-9 w-64"
+                        />
+                        {search && (
+                            <button
+                                onClick={() => {
+                                    setSearch('');
+                                    setSearchQuery('');
+                                }}
+                                className="absolute inset-y-0 right-0 pr-3 flex items-center hover:text-foreground text-muted-foreground transition-colors"
+                            >
+                                <X className="h-4 w-4" />
+                            </button>
                         )}
                     </div>
-                )}
-                <Dropdown setLimit={handleLimitChange} currentLimit={limit} />
+                </div>
+                <div className='flex items-center justify-between w-full'>
+                    {pagination && (
+                        <div className="text-sm text-muted-foreground">
+                            Showing {(pagination.currentPage - 1) * limit + 1}{' '}
+                            to{' '}
+                            {Math.min(
+                                pagination.currentPage * limit,
+                                pagination.totalCount
+                            )}
+                        </div>
+                    )}
+                    <Dropdown
+                        setLimit={handleLimitChange}
+                        currentLimit={limit}
+                    />
+                </div>
             </div>
             <div className="flex justify-center items-center gap-2">
                 <CreateShortUrl text="Create" />
--- client/src/Components/Table/TableRows.tsx
+++ client/src/Components/Table/TableRows.tsx
@@ -3,6 +3,12 @@ import Copy from '../Utils/Copy';
 import Delete from '../Utils/Delete';
 import Share from '../Utils/Share';
 import type { Url } from '@/types/url';
+import { EllipsisVertical, ExternalLink } from 'lucide-react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from '@/Components/ui/dropdown-menu';
 
 interface TableRowsProps {
     url: Url;
@@ -29,73 +35,144 @@ const TableRows = ({
             .replace(',', '');
     };
     return (
-        <div
-            className=" bg-background text-xs text-foreground border-t table-grid"
-            key={url.id}
-        >
-            <div className="flex items-center px-4 py-4 gap-2">
-                <Link
-                    to={url.shortLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="truncate"
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    {url.shortLink}
-                </Link>
-                <div className="flex items-center">
-                    <Copy url={url.shortLink} />
+        <>
+            <div
+                className="bg-background text-xs text-foreground border-t table-grid"
+                key={url.id}
+            >
+                <div className="flex items-center px-4 py-4 gap-2">
+                    <Link
+                        to={url.shortLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="truncate"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {url.shortLink}
+                    </Link>
+                    <div className="flex items-center">
+                        <Copy url={url.shortLink} />
+                    </div>
+                </div>
+
+                <div className="flex items-center px-4 py-4">
+                    <Link
+                        to={url.originalUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="truncate"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        {url.originalUrl}
+                    </Link>
+                </div>
+
+                <div className=" px-4 py-4 flex items-center justify-center">
+                    <Link
+                        to={`visit/${url.id}`}
+                        key={url.shortLink}
+                        onMouseEnter={() => handleMouseEnter(url.id)}
+                        className="hover:text-secondary-foreground transition-all duration-200"
+                    >
+                        {' '}
+                        View
+                    </Link>
+                </div>
+
+                <div className="px-4 py-4 flex items-center justify-center">
+                    <p>{url._count.visits}</p>
+                </div>
+
+                <div className="px-4 py-4 flex items-center justify-center">
+                    <p>{formatDate(url.createdAt)}</p>
+                </div>
+
+                <div className="flex items-center justify-center px-4 py-4 gap-4">
+                    <Delete
+                        url={url}
+                        deleteDialog={dialog}
+                        setDeleteDialog={setDialog}
+                    />
+                    <Share value={url.shortLink} />
                 </div>
             </div>
+            <div
+                key={url.id}
+                className="relative h-full flex justify-between items-center border  rounded hover:bg-muted/20 lg:hidden"
+            >
+                <div className="w-[50vw] h-fit flex flex-col py-4 px-4 gap-1">
+                    <div className="flex flex-col items-start">
+                        <p className="text-xs text-muted-foreground">
+                            Short Url
+                        </p>
+                        <div className="flex items-center gap-2 w-full">
+                            <Link
+                                to={url.shortLink}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="truncate text-sm"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                {url.shortLink}
+                            </Link>
+                            <div className="flex items-center">
+                                <Copy url={url.shortLink} />
+                            </div>
+                        </div>
+                    </div>
 
-            <div className="flex items-center px-4 py-4">
-                <Link
-                    to={url.originalUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="truncate"
-                    onClick={(e) => e.stopPropagation()}
-                >
-                    {url.originalUrl}
-                </Link>
-            </div>
+                    <div className="flex flex-col items-start gap-2 w-full">
+                        <p className="text-xs text-muted-foreground">
+                            Original Url
+                        </p>
+                        <Link
+                            to={url.originalUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sm truncate w-full"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            {url.originalUrl}
+                        </Link>
+                    </div>
+                </div>
 
-            <div className=" px-4 py-4 flex items-center justify-center">
-                <Link
-                    to={`visit/${url.id}`}
-                    key={url.shortLink}
-                    onMouseEnter={() => handleMouseEnter(url.id)}
-                    className="hover:text-secondary-foreground transition-all duration-200"
-                >
-                    {' '}
-                    View
-                </Link>
-            </div>
+                <div className="w-full h-full flex flex-col items-end justify-between py-4 px-4 gap-5">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className=" cursor-pointer">
+                            <EllipsisVertical size={16} />
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-fit flex flex-col items-center justify-center gap-2">
+                            <Delete
+                                url={url}
+                                deleteDialog={dialog}
+                                setDeleteDialog={setDialog}
+                            />
+                            <Share value={url.shortLink} />
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <div className=" flex items-center justify-center">
+                        <Link
+                            to={`visit/${url.id}`}
+                            key={url.shortLink}
+                            onMouseEnter={() => handleMouseEnter(url.id)}
+                            className="hover:text-secondary-foreground transition-all duration-200"
+                        >
+                            {' '}
+                            <ExternalLink size={16} />
+                        </Link>
+                    </div>
 
-            <div className="px-4 py-4 flex items-center justify-center">
-                <p>{url._count.visits}</p>
-            </div>
+                    <p className="text-xs text-muted-foreground border py-1 px-3 rounded-full">
+                        Total Visit {url._count.visits}
+                    </p>
 
-            <div className="px-4 py-4 flex items-center justify-center">
-                <p>{formatDate(url.createdAt)}</p>
+                    {/* <div className="px-4 py-4 flex items-center justify-center">
+                    <p>{formatDate(url.createdAt)}</p>
+                </div> */}
+                </div>
             </div>
-
-            <div className="flex items-center justify-center px-4 py-4 gap-4">
-                {/* <button className="cursor-pointer">
-                    <Settings
-                        size={20}
-                        strokeWidth={1}
-                        className="hover:rotate-45 transition-transform duration-300"
-                    />
-                </button> */}
-                <Delete
-                    url={url}
-                    deleteDialog={dialog}
-                    setDeleteDialog={setDialog}
-                />
-                <Share value={url.shortLink} />
-            </div>
-        </div>
+        </>
     );
 };
 
